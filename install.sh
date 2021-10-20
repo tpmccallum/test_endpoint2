e10de9d116c119f9b73b125040449367c5de8858
Timothy McCallum <tpmccallum@users.noreply.github.com>
2021-10-20 11:37:13 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -75,7 +75,6 @@ get_latest_release() {
     local res
     res=$(git ls-remote --refs --tags "https://github.com/$1.git" |
         cut -d '/' -f 3 |
-        tr '-' '~' |
         sort --version-sort |
         tail -1)
     echo "$res"
